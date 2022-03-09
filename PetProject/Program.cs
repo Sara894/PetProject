900c70e990799188964ecf1ab7ab74cfccbddc33
Sara894 <t48250klo@gmail.com>
2022-03-09 11:26:21 +0300

    Добавьте файлы проекта.

---
+++ PetProject/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace PetProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Программа для подбора фильмов!");
+            string genre = Console.ReadLine();
+            Console.WriteLine("Рекомендую следующие фильмы: ");
+
+            if(genre == "Комедия")
+            {
+                Console.WriteLine("Иван Васильевич меняет профессию.");
+                Console.WriteLine("Моя прекрасная няня");
+            }
+            else if(genre == "Фантастика")
+            {
+                Console.WriteLine("Приключения Электроника");
+                Console.WriteLine("Люди Икс");
+            }
+            else
+            {
+                Console.WriteLine("Зеленая миля");
+                Console.WriteLine("В бой идут одни старики");
+            }
+        }
+    }
+}
